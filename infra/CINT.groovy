9e9b5a7bd9d9a6900ba0dd1698d58b5ff17b0a4e
Deyvid Lins <deyvidg.lins@gmail.com>
2021-02-22 22:02:50 -0300

    alteraçao

--- infra/CINT.groovy
+++ infra/CINT.groovy
@@ -16,7 +16,7 @@ node ('master'){
             sh 'pwd'
         }
 
-        tage('Instalação do sfdx'){
+        stage('Instalação do sfdx'){
             sh 'wget https://developer.salesforce.com/media/salesforce-cli/sfdx-cli/channels/stable/sfdx-cli-linux-x64.tar.xz'
             sh 'mkdir sfdx'
             sh 'ls'
